b9acfbd560474180a44f69a48e637311ce796e9d
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-07-12 21:54:36 +1200

    mobile process close to design

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const email = document.getElementById("email");
 const form = document.getElementById("form");
 const successMessage = document.getElementById("success-message");
 const errorMessage = document.getElementById("error-message");
+const submitBtn = document.getElementById("submitBtn");
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function isValidEmail(value) {
@@ -11,16 +12,26 @@ function isValidEmail(value) {
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // Always prevent default form submission
   errorMessage.innerText = "";
+  const emailValue = email.value;
 
-  if (!email.value) {
-    errorMessage.innerText = "Email is required.";
+  if (!emailValue) {
+    errorMessage.innerText = "Email is required";
     return;
   }
-
-  if (!isValidEmail(email.value)) {
+  if (!isValidEmail(emailValue)) {
     errorMessage.innerText = "Valid email required";
+    email.style.borderColor = "hsl(4, 100%, 67%)";
+    email.style.color = "hsl(4, 100%, 67%)";
+    email.style.backgroundColor = "hsla(5, 100%, 95%, 1.00)";
     return;
   }
   successMessage.style.display = "block";
   form.style.display = "none";
+});
+
+email.addEventListener("focus", () => {
+  email.style.borderColor = "hsl(0, 0%, 58%)";
+  email.style.color = "";
+  email.style.backgroundColor = "";
+  
 });
